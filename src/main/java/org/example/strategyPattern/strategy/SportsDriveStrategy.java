c455d1f6fe084f367412c685877cefd7ef24014f
Venkatesh <venkypoluri898@gmail.com>
2024-10-06 23:34:09 +0530

    Committing java code for  Strategy  Design Pattern.

--- src/main/java/org/example/strategyPattern/strategy/SportsDriveStrategy.java
+++ src/main/java/org/example/strategyPattern/strategy/SportsDriveStrategy.java
@@ -3,6 +3,6 @@ package org.example.strategyPattern.strategy;
 public class SportsDriveStrategy implements DriveStrategy{
     @Override
     public void drive() {
-        System.out.println("Sports Drive Capability");
+        System.out.println("Sports Drive capability");
     }
 }
